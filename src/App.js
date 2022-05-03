4ae591362aefa01c59c2535de0e4280885a0791e
Dandre Bell <djbell1999@comcast.net>
2022-05-03 14:37:09 -0400

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import './App.css';
 import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom"
-
+import Home from "./Pages/Home"
+import About from "./Pages/About"
+import Resume from "./Pages/Resume"
+import Projects from "./Pages/Projects"
+import Contact from "./Pages/Contact"
+import Error from "./Pages/ErrorPage"
 
 function App() {
   return (
-
-    <nav>
-
-    </nav>
+    <Router>
+      <nav>
+        
+      </nav>
+      <Routes>
+        <Route path='/' element={Home}/>
+        <Route path='/About' element={About}/>
+        <Route path='/Resume' element={Resume}/>
+        <Route path='/Projects' element={Projects}/>
+        <Route path='/Contact' element={Contact}/>
+        <Route path='*/' element={Error}/>
+      </Routes>
+    </Router>
   );
 }
 
