c55a16ca6db93f017a1f4554247359fe63373ed9
Dandre Bell <djbell1999@comcast.net>
2022-05-03 00:49:15 -0400

    Created page folder and files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom"
+
 
 function App() {
   return (
 
+    <nav>
+
+    </nav>
   );
 }
 
