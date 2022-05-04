a35d3bcba700b16b258a2f0892d736782160b8bf
Dandre Bell <djbell1999@comcast.net>
2022-05-04 12:56:28 -0400

    Added icons and links

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ import Projects from "./Pages/Projects"
 import Contact from "./Pages/Contact"
 import Error from "./Pages/ErrorPage"
 import './Styles/MainStyles.css'
-
+import IconButton from '@mui/material/IconButton'
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 const linkStyle = {
   padding: 20,
@@ -16,15 +18,28 @@ const linkStyle = {
   fontSize: 22,
 };
 
+const iconStyle = {
+  color: 'white',
+  fontSize: 38,
+  marginLeft: 10,
+  marginRight:10
+};
+
 function App() {
   return (
     <Router style={{backgroundColor: "#3E3E3E"}}>
       <div className="NavBar" style={{height: (window.innerHeight * 0.05)}}>
+        <div className='PageList'>
           <Link to="/" style={linkStyle}>Home</Link>
           <Link to="/About" style={linkStyle}>About me</Link>
           <Link to="/Resume" style={linkStyle}>Resume</Link>
           <Link to="/Projects" style={linkStyle}>Projects</Link>
           <Link to="/Contact" style={linkStyle}>Contact me</Link>
+        </div>
+        <div className='IconList'>
+          <IconButton onClick={() => window.open("https://github.com/Dandre-Bell?tab=repositories")}><GitHubIcon style={iconStyle}/></IconButton>
+          <IconButton onClick={() => window.open("https://www.linkedin.com/in/dandre-bell-56204219a/")}><LinkedInIcon style={iconStyle}/></IconButton>
+        </div>
       </div>
 
       <Routes>
