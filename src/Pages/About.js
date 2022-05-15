9010ea738814d933a09ebab0379a5d6096b8ab2b
Dandre Bell <djbell1999@comcast.net>
2022-05-14 20:36:38 -0400

    Linked to api

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,16 +1,16 @@
-import React from 'react'
+import {React, useEffect, useState} from 'react'
+import { GetBio } from '../Controllers/PageApiController'
 import '../Styles/AboutBoxStyle.css'
 
 function About(){
+    const [text, setText] = useState('')
+    useEffect(() => {setText(GetBio())}, []);
+
     return(
         <div style={{height: (window.innerHeight * 0.95)}} className="PageBackground">
             <div className='BoxStyle'>
                 <h1>
-                    I'm a recent graduate of the University of Pittsburgh at Johnstown
-                     with a degree in computer science. During my time at the university
-                     I was president of the martial arts club for 4 years and acted in several theatrical productions.
-                     As a software engineer I am looking to specialize in full-stack web development and virtual
-                      and augmented reality development.
+                    {text}
                 </h1>
             </div>
         </div>
